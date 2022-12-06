155c58c31d23107f5637b191d484977c6592caa8
puffin-beta <80365550+puffin-beta@users.noreply.github.com>
2022-12-06 11:17:42 -0800

    Update contact.php

--- contact-page/contact.php
+++ contact-page/contact.php
@@ -1,3 +1,9 @@
 <?php
-	echo "This works!";
+	echo "<h1>Contact Form</h1>
+		  <form action=\"contact1.php\" method=\"get\">
+			Name: <input type=\"text\" name=\"name\" required></input><br/>
+			Email: <input type=\"text\" name=\"email\" required></input><br/>
+			Your Concern / Feedback: <input type=\"comment\" name=\"concern\" required></input><br/>
+			<input type=\"submit\" value=\"submit\"></input>
+		  </form>";
 ?>
