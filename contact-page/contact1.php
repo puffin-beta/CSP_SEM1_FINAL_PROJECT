34af44ec51d5c40fc430ee8f290f6e376179b3e0
puffin-beta <80365550+puffin-beta@users.noreply.github.com>
2022-12-06 11:17:45 -0800

    Update contact1.php

--- contact-page/contact1.php
+++ contact-page/contact1.php
@@ -1,9 +1,18 @@
-<!DOCTYPE html>
-<html>
-	<head></head>
-	<body>
-		<?php
-			echo "This Works!!";
-		?>
-	</body>
-</html>
+<?php
+	function test_input($data){
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
+	$name = test_input($_GET['name']);
+	echo $name;
+	$pattern = "/^[a-zA-Z-' ]*$/";
+	if (!preg_match($pattern,$name)) {
+		$nameErr = "Only letters and white space allowed";
+		header("Location: contact.php");
+	}
+	else{
+		echo "Thank you for sharing! Your response will be acknowledged soon.\n";
+	}
+?>
