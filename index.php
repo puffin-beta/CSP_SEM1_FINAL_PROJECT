926a798cd83176427bb547741dcf5e64c23e7e76
puffin-beta <80365550+puffin-beta@users.noreply.github.com>
2022-12-06 22:19:13 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1" />
+	<link href="index.css" rel="stylesheet">
+	<head>
+		<title>Minecraft Combat Guide</title>
+	</head>
+	<header>
+		<img src="background.png" class="bg">
+		<h1>Minecraft Combat Guide</h1>
+		<p style="color:white; text-align:center;">This is the Minecraft Combat guide. It contains information about the armors, weapons, mobs, and enchantments of Minecraft.</p>
+	</header>
+	<body>
+		<div class="table-container">
+			<div class="table-item">
+				<div class="icon-container">
+					<a href="weapons-page-assets/weapons.php">
+						<img class="icon" src="weapons-icon.png" alt="Weapons">
+					</a>
+				</div>
+			</div>
+			<div class="table-item">
+				<a href="armor-assets/armor.php">
+					<div class="icon-container"><img class="icon" src="armor-icon.png" alt="Armor"></div>
+				</a>
+			</div>
+			<div class="table-item">
+				<div class="icon-container">
+					<a href="mobs-page-assets/mobs.php">
+						<img class="icon" src="mobs-icon.png" alt="Mobs">
+					</a>
+				</div>
+			</div>
+			<div class="table-item">
+				<div class="icon-container"><img class="icon" src="enchantments-icon.png" alt="Enchantments"></div>
+			</div>
+		</div><br/>
+		<button id="contact-prompt"><a href="contact-page/contact.php">CLICK HERE TO SUBMIT QUESTIONS AND/OR FEEDBACK!</a></button>
+	</body>
+</html>
