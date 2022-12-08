024694ee4ed9fa5fd7282449d41b939d68f30fb3
brucerili <rilibruce@gmail.com>
2022-12-08 12:55:05 -0800

    Update weapons.php

--- weapons-page-assets/weapons.php
+++ weapons-page-assets/weapons.php
@@ -10,13 +10,15 @@ body {
 * :not(img){
 	z-index: 1;
 }
+img
 </style>
 <body>
 <h1 style="color:White; font-family: courier; font size: 150%;">Weapons of Minecraft</h1>
 <hr>
 <p style="color:White; z-index:1">The following page has information on the weapons of Minecraft.</p>
 <a href="swordsubpage/swordsubpage.php">
-<img src="weaponsswordlink.png" alt="HTML tutorial" style="width:200px;height:200px;"></a>
-
+<img src="weaponsswordlink.png" alt="SWORDS" style="width:200px;height:200px;"></a>
+<a href="bowsubpage/bowsubpage.php">
+<img src="bowsubpageicon.png" alt="BOWS" style="width:200px;height:200px;"></a>
 </body>
 </html>
