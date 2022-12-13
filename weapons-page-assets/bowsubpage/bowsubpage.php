f978e7c2011c5faece2fdf6bc0482c6f65af2d6f
brucerili <rilibruce@gmail.com>
2022-12-13 12:17:09 -0800

    Update bowsubpage.php

--- weapons-page-assets/bowsubpage/bowsubpage.php
+++ weapons-page-assets/bowsubpage/bowsubpage.php
@@ -17,6 +17,8 @@ img
 
 <h1 style="z-index:1; color:White">Bow</h1>
 <p style= "color:White">This is a paragraph.</p>
+<img src="image11.png" alt bow="Bow stats"> <style="width:200px;height:200px;"></a>
+<a href="bowsubpage/bowsubpage.php">
 
 </body>
 </html>
