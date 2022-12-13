d509a14637b0794777a9a6432461428ba575f079
brucerili <rilibruce@gmail.com>
2022-12-13 11:41:44 -0800

    Update bowsubpage.php

--- weapons-page-assets/bowsubpage/bowsubpage.php
+++ weapons-page-assets/bowsubpage/bowsubpage.php
@@ -1,7 +1,18 @@
 <!DOCTYPE html>
 <html>
+<style>
+body {
+	background-image: url('bowbackground.png');
+	background-repeat: no-repeat;
+	background-attachment: fixed;
+	background-size: 100% 100%;
+}
+* :not(img){
+	z-index: 1;
+}
+img
 <head>
-<title>Page Title</title>
+<title>Bows</title>
 </head>
 <body>
 
