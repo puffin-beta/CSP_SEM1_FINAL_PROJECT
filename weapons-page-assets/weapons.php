d7eac1b40a56d0e30c48fe718225249334ee880e
puffin-beta <80365550+puffin-beta@users.noreply.github.com>
2022-12-13 12:25:35 -0800

    Update weapons.php

--- weapons-page-assets/weapons.php
+++ weapons-page-assets/weapons.php
@@ -17,10 +17,33 @@ img
 <hr>
 <p style="color:White; z-index:1">The following page has information on the weapons of Minecraft.</p>
 <a href="swordsubpage/swordsubpage.php">
-<img src="weaponsswordlink.png" alt="SWORDS" style="width:200px;height:200px;"></a>
+<!--img src="weaponsswordlink.png" alt="SWORDS" style="width:200px;height:200px;"></a>
 <a href="bowsubpage/bowsubpage.php">
 <img src="bowsubpageicon.png" alt="BOWS" style="width:200px;height:200px;"></a>
 <a href="tridentsubpage/tridentsubpage.php">
-<img src="tridenthomepageweapon.png" alt="TRIDENT" style="width:200px;height:200px;"></a>
+<img src="tridenthomepageweapon.png" alt="TRIDENT" style="width:200px;height:200px;"></a-->
+<?php 
+	include("../menu.php");
+	$images = ["bowsubpageicon.png","tridenthomepage.png","weaponsswordlink.png"];
+	echo "<div class=\"table-container\">
+			<div class=\"table-item\">
+				<div class=\"icon-container\">
+					<a href=\"swordsubpage/swordsubpage.php\">
+						<img class=\"icon\" src=\"$images[2]\" alt=\"Swords\">
+					</a>
+				</div>
+			</div>
+		  </div>
+		  <div class=\"table-container\">
+			<div class=\"table-item\">
+				<div class=\"icon-container\">
+					<a href=\"bowsubpage/bowsubpage.php\">
+						<img class=\"icon\" src=\"$images[0]\" alt=\"Bows\">
+					</a>
+				</div>
+			</div>
+		  </div>
+		  ";
+?>
 </body>
 </html>
