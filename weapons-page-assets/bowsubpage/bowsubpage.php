74b079c9419deb1c4821e6167e8a8650deb39a89
brucerili <rilibruce@gmail.com>
2022-12-15 10:58:43 -0800

    Update bowsubpage.php

--- weapons-page-assets/bowsubpage/bowsubpage.php
+++ weapons-page-assets/bowsubpage/bowsubpage.php
@@ -18,13 +18,13 @@ img{
 <title>Bows</title>
 
 <h1 style="z-index:1; color:White">Bow and Crossbow</h1>
-<p style= "color:White">This is a paragraph.</p>
 <img src="../mainbow.png" alt="Bow main image"> <style="width:200px;height:200px;"></a>
+<P style= "color:White">Here are some of the common stats of a bow</p>
 <img src="../image12.png" alt="Bow stats"> <style="width:200px;height:200px;"></a>
+<P style= "color:White"> How long you draw affects the power of the arrow.</p>
 <img src="../image14.png" alt="Bow crafting"> <style="width:200px;height:200px;"></a>
 <P style= "color:White"> <br> You need 3 sticks, and 3 spider </br> webs in order to craft a bow, arrange in <br> this order to craft.<br></p>
-
-
+<img src="../crossbowstats.png" alt="Bow crafting"> <style="width:100px;height:100px;"></a>
 
 </body>
 </html>
