da64b72afd2f96439814597cee0115053d613535
brucerili <rilibruce@gmail.com>
2022-12-15 12:46:44 -0800

    Update bowsubpage.php

--- weapons-page-assets/bowsubpage/bowsubpage.php
+++ weapons-page-assets/bowsubpage/bowsubpage.php
@@ -17,12 +17,14 @@ body {
 
 <h1 style="z-index:1; color:White">Bow and Crossbow</h1>
 <img src="../mainbow.png" alt="Bow main image"> <style="width:200px;height:200px;"></a>
-<P style= "color:White">Here are some of the common stats of a bow</p>
+<P style= "color:White">Here are some of the common stats of a bow.</p>
 <img src="../image12.png" alt="Bow stats"> <style="width:200px;height:200px;"></a>
 <P style= "color:White"> How long you draw affects the power of the arrow.</p>
 <img src="../image14.png" alt="Bow crafting" style="width:200px;height:200px;"></a>
 <P style= "color:White"> <br> You need 3 sticks, and 3 spider </br> webs in order to craft a bow, arrange in <br> this order to craft.<br></p>
-<div class="right"> <img src="../crossbowstats.png" alt="Bow crafting" style="width:450px;height:250px;"></a> </div>
-
+<img src="../image6.png" alt="crossbow main"> <style="width:200px;height:200px;"></a>
+<p style="color:White"> Here are the common stats of a crossbow</p>
+<img src="../crossbowstats.png" alt="Bow crafting" style="width:450px;height:250px;"></a> </div>
+<P style= "color:White"> The chances of finding crossbows in the following locations varies per edition.</p>
 </body>
 </html>
