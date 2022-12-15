960dff9b60049aa03b0ae43db9ee404450e7e726
brucerili <rilibruce@gmail.com>
2022-12-15 10:35:49 -0800

    Update bowsubpage.php

--- weapons-page-assets/bowsubpage/bowsubpage.php
+++ weapons-page-assets/bowsubpage/bowsubpage.php
@@ -15,11 +15,12 @@ img
 <body>
 <title>Bows</title>
 
-<h1 style="z-index:1; color:White">Bow</h1>
+<h1 style="z-index:1; color:White">Bow and Crossbow</h1>
 <p style= "color:White">This is a paragraph.</p>
 <img src="../mainbow.png" alt="Bow main image"> <style="width:200px;height:200px;"></a>
-<img src="../image14.png" alt="Bow crafting"> <style="width:200px;height:200px;"></a>
 <img src="../image12.png" alt="Bow stats"> <style="width:200px;height:200px;"></a>
+<img src="../image14.png" alt="Bow crafting"> <style="width:200px;height:200px;"></a>
+
 
 
 
