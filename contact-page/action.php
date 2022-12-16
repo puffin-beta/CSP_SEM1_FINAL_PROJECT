bc10e9e1ef7c0deefe60b631dcd5e0d7fa5d7bb1
puffin-beta <80365550+puffin-beta@users.noreply.github.com>
2022-12-15 23:27:07 -0800

    Update action.php

--- contact-page/action.php
+++ contact-page/action.php
@@ -1,20 +1,9 @@
 <!DOCTYPE html>
 <html>
 	<head></head>
-	<body onload="run()">
-		<p>Redirecting in<p id="counter">5</p>
-		</p>
+	<body>
+		<?php
+			echo "Thanks for submitting!";
+		?>
 	</body>
-	<script type="text/javascript">
-		var counterText = document.getElementById("counter");
-		var counter = 5;
-		function countdown(var count){
-			var count = count;
-			count--;
-			counterText.innerHTML = count;
-		}
-		function run(){
-			setInterval(countdown(counter),1000);
-		}
-	</script>
 </html>
