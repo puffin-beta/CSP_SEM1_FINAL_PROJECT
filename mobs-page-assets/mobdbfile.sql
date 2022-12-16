1d9b287ef67318eece5c1af597a52db33571a201
puffin-beta <80365550+puffin-beta@users.noreply.github.com>
2022-12-15 18:03:59 -0800

    Update mobdbfile.sql

--- mobs-page-assets/mobdbfile.sql
+++ mobs-page-assets/mobdbfile.sql
@@ -1,2 +1,12 @@
+CREATE DATABASE MOB_DATA;
 USE MOB_DATA;
-SHOW VARIABLES LIKE 'secure_file_priv';
+CREATE TABLE mob_data(
+	mobId int,
+    mobName varchar(255),
+    mobHealth varchar(255),
+    mobSpawn varchar(255),
+    mobDamage varchar(255),
+    mobLootDrops varchar(255),
+    mobSpecialNotes varchar(255)
+);
+LOAD DATA INFILE "/path/to/MOB_DATA-Sheet1.csv" INTO TABLE mob_data;
