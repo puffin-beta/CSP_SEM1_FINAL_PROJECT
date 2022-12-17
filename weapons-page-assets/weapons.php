572067ce9f8de4a91e0c01ac2135a9a468f5e3fa
puffin-beta <80365550+puffin-beta@users.noreply.github.com>
2022-12-16 20:24:19 -0800

    []

--- weapons-page-assets/weapons.php
+++ weapons-page-assets/weapons.php
@@ -14,18 +14,17 @@ img
 </style>
 <body>
 <h1 style="color:White; font-family: courier; font size: 150%;">Weapons of Minecraft</h1>
-<hr>
+<br>
 <p style="color:White; z-index:1">The following page has information on the weapons of Minecraft.</p>
 <a href="swordsubpage/swordsubpage.php">
-<img src="weaponsswordlink.png" alt="SWORDS" style="width:200px;height:200px;"></a>
-<a href="swordsubpage/swordsubpage.php">
->>>>>>> 68e5b98fc41647d00c84c88675020261de741156
-<img src="weaponsswordlink.png" alt="SWORDS" style="width:200px;height:200px;"></a>
->>>>>>> 8b989a7799becc5bd914f6d35eadcfac3d0044ae
+	<img src="weaponsswordlink.png" alt="SWORDS" style="width:200px;height:200px;">
+</a>
 <a href="bowsubpage/bowsubpage.php">
-<img src="bowsubpageicon.png" alt="BOWS" style="width:200px;height:200px;"></a>
+	<img src="bowsubpageicon.png" alt="BOWS" style="width:200px;height:200px;">
+</a>
 <a href="tridentsubpage/tridentsubpage.php">
-<img src="tridenthomepageweapon.png" alt="TRIDENT" style="width:200px;height:200px;"></a>
+	<img src="tridentsubpage/tridenthomepageweapon.png" alt="TRIDENT" style="width:200px;height:200px;">
+</a>
 
 </body>
 </html>
