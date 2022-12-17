a857b14688f9108c5b961ed884c30e467ac50e45
rajveerkharod <111790153+rajveerkharod@users.noreply.github.com>
2022-12-16 19:38:19 -0800

    o

--- armor-assets/armor.php
+++ armor-assets/armor.php
@@ -1,10 +1,34 @@
-<!DOCTYPE html>
 <html>
+<style>
+body {
+	background-image: url('armback.jpg');
+	background-repeat: no-repeat;
+	background-attachment: fixed;
+	background-size: 100% 100%;
+}
+* :not(img){
+	z-index: 1;
+}
+@font-face{
+	font-family: Minecraft;
+	src: url("../custom-font.ttf");
+}
+</style>
 <body>
+<title>Bows</title>
 
-<h1>My First Heading</h1>
-<img src = >
-<p>My first paragraph.</p>
-
+<h1 style="z-index:1; color:White; font-family:Minecraft; font-weight:lighter; font-size:50;text-align:center">ARMOR</h1>
+<img src="arm1.png" alt="Armor main image"> <style="width:200px;height:200px;"></a>
+<P style= "color:Green">How to craft a Helmet in Minecraft.</p>
+<img src="arm2.png" alt="Sword stats"> <style="width:200px;height:200px;"></a>
+<P style= "color:Green"> Here are the stats for the rarity, and the durability <br/> for each sword variant.</p>
+<img src="arm3.png" alt="Sword main"</a>
+<P style= "color:Green">You need 1 stick, and 2 pieces of your desired material.</p>
+<img src="arm4.png" alt="Sword main"> <style="width:200px;height:200px;"></a>
+<p style="color:Green">How to craft a pair of boots in Minecraft</p>
+<img src="arm5.png" alt="Sword main"</div>
+<P style= "color:Green">Demonstration on how to upgrade armor to the Powerful Netherite</p>
+<img src="arm6.png" alt="Sword main"> <style="width:200px;height:200px;"></a>
+<P style="color:Green">These show the different protection percentage for each armor variant</P>
 </body>
 </html>
