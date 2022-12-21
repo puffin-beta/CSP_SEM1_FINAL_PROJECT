159966b09ab8a021b29787c991cc2b82f00ed378
rajveerkharod <111790153+rajveerkharod@users.noreply.github.com>
2022-12-20 18:38:15 -0800

    Update enchantments.php

--- enchantments-page-assets/enchantments.php
+++ enchantments-page-assets/enchantments.php
@@ -58,4 +58,15 @@
 			</tr>
 		</table>
 	</body>
+	<p style="color:White">Mending:<br/>What mending does is it helps repair your weapons as you collect xp orbs. This is helpful as it lets keep your weapons for longer, without having to craft it over and over again)This has only 1 level in the enchantment.</p>
+	<p style="color:White">Unbreaking:<br/>What this does is it increases the life of your weapon by increasing the durability of it. It goes up to 3 levels.</p>
+	<p style="color:White">Fortune:<br/>This increases the amount of a drop which you can get,or the percent chance of what you could possible get. This can go to level of 3.</p>
+	<p style="color:White">Aqua Affinity:<br/>What this does is it lets you mine when you are underwater at a higher speed, this goes to the level of 1.</p>
+	<p style="color:White">Feather Falling:<br/>This lowers the amount of damage which falling from a distance can do.You could put it to level 4 in enchantment.)</p>
+	<p style="color:White">Looting:<br/>This gives larger drops, and increases the chances for the player with the enchantment to obtain a more rare item.This goes to level 3.</p>
+	<p style="color:White">Silk Touch:<br/>This enchantment gives back the block instead of the items it would normally drop, this can go to level 1.</p>
+	<p style="color:White">Efficiency:<br/>What efficiency does is it lets the player with the enchantment mine at a higher than normal level, this is beneficial as it uses less time, it can go up to level 5.</p>
+	<p style="color:White">Fire Protection:<br/>What Fire Protection does is it lowers how much damage which fire can do to the player with the enchantment.This can go to level 5.</p>
+	<p style="color:White">Knockback:<br/>What knockback does is it lets the player knock their opponent further back, so the mob would go further back than normal.This goes to level 2.</p>
+</p>
 </html>
