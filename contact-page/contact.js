7b27bab790a600bb6771aae5688d65dc63fa83c0
puffin-beta <80365550+puffin-beta@users.noreply.github.com>
2022-12-21 00:09:23 -0800

    Update contact.js

--- contact-page/contact.js
+++ contact-page/contact.js
@@ -1,8 +1,11 @@
+// variables to use in JS
 var body = document.getElementById("body");
 var chbut = document.getElementById("modeswitch");
+// Redirect to home page
 function back_to_home(){
 	location.replace("../index.php");
 }
+// Change between light and dark mode
 function changetheme() {
   body.classList.toggle("lightMode");
   chbut.classList.toggle("lightMode");
